dd258f98fc315dc8a17e5712cd32886e6a4751fa
munene19 <munenexv@gmail.com>
2020-01-05 17:07:29 +0300

    add article sources

--- run.py
+++ run.py
@@ -7,7 +7,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     newsapi = NewsApiClient(api_key="65034e770a4f45ffaeafc84d023e3ca4")
-    Headlines = newsapi.get_top_headlines(sources=bbc - news)
+    Headlines = newsapi.get_top_headlines(sources=bbc-news)
 
 
     articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
