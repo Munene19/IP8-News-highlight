081643e38b6441af278f8cf6c4e57dec82202721
munene19 <munenexv@gmail.com>
2020-01-05 17:01:10 +0300

    added news API

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from Flask import flask
 from newsapi import NewsApiClient
 
-app=Flask(__name__)
-
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     newsapi = NewsApiClient(api_key="65034e770a4f45ffaeafc84d023e3ca4")
+    Headlines = newsapi.get_top_headlines(sources=bbc - news)
+
+
+    articles = topheadlines['articles']
