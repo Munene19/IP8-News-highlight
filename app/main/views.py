fd6820f2b1563b2731912f7105c43840242aecf2
munene19 <munenexv@gmail.com>
2020-01-06 09:33:31 +0300

    views file debugged

--- app/main/views.py
+++ app/main/views.py
@@ -2,19 +2,29 @@ from flask import render_template, request, redirect, url_for
 from . import main
 from ..requests import get_topstories, get_categories, get_newsupdates
 
- #Views
+
+# Views
 @main.route('/')
 def index():
-     '''
+
+    '''
     View root page function that returns the index page and its data
     '''
-     #Getting top news and categorically arranged news
-     top_articles = get_topstories('google-news')
-     print(top_articles)
-     biz_articles = get_categories('business')
-     tech_articles = get_categories('technology')
-     ent_articles = get_categories('entertainment')
-     sprt_articles = get_categories('sports')
-     title = 'News highlight'
-     return render_template('index.html', title = title, google_news = top_articles, biz = biz_articles, tech = tech_articles, ent = ent_articles, sprt = sprt_articles)
 
+    # Getting top news and categorically arranged news
+    top_stories = get_topstories('google-news')
+    print(top_stories)
+    title = 'News highlights'
+    tech = get_categories('technology')
+    entertainment = get_categories('entertainment')
+    business = get_categories('business')
+    sports = get_categories('sports')
+
+    return render_template('index.html', title=title, google_news=top_stories, biz=business, tech=tech,
+                           ent=entertainment, sprt=sports)
+
+    # @main.route('/update/<id>')
+    # def article(id):
+    #     detz_articles = get_newsupdates(id)
+    #     print(detz_articles)
+    #     return render_template('news.html', detz=detz_articles)
