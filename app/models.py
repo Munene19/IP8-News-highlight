654d574fdb00bdbba1141af3291c4c6db4a29101
munene19 <munenexv@gmail.com>
2020-01-07 09:58:14 +0300

    landing debugged

--- app/models.py
+++ app/models.py
@@ -1,39 +1,39 @@
-class Categories:
-    """
-    Categories class to define categories objects
-    """
-
-    def __init__(self, id, name, description, url):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.url = url
-
-
-class Topstories:
-    """
-    Topstories class to define topstories objects
-    """
-
-    def __init__(self, name, author, title, description, urlToImage, url):
-        self.name = name
-        self.author = author
-        self.title = title
-        self.description = description
-        self.urlToImage = urlToImage
-        self.url = url
-
-
-class NewsUpdate:
-    """
-    NewsUpdate class to define categories objects
-    """
-
-    def __init__(self, id, author, title, description, url, urlToImage, publishedAt):
-        self.id = id
-        self.author = author
-        self.title = title
-        self.description = description
-        self.url = url
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
+# class Categories:
+#     """
+#     Categories class to define categories objects
+#     """
+#
+#     def __init__(self, id, name, description, url):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.url = url
+#
+#
+# class Topstories:
+#     """
+#     Topstories class to define topstories objects
+#     """
+#
+#     def __init__(self, name, author, title, description, urlToImage, url):
+#         self.name = name
+#         self.author = author
+#         self.title = title
+#         self.description = description
+#         self.urlToImage = urlToImage
+#         self.url = url
+#
+#
+# class NewsUpdate:
+#     """
+#     NewsUpdate class to define categories objects
+#     """
+#
+#     def __init__(self, id, author, title, description, url, urlToImage, publishedAt):
+#         self.id = id
+#         self.author = author
+#         self.title = title
+#         self.description = description
+#         self.url = url
+#         self.urlToImage = urlToImage
+#         self.publishedAt = publishedAt
--- manage.py
+++ manage.py
@@ -102,5 +102,34 @@ def BBC():
     return render_template('BBC.html', context=mylist)
 
 
+
+@app.route('/Axios')
+def Axios():
+    newsapi = NewsApiClient(api_key="fc44e252019a4642b8194a9006c9a279")
+    topstories = newsapi.get_top_headlines(sources="axios")
+
+    articles = topstories['articles']
+    desc = []
+    news = []
+    img = []
+    pubAt = []
+    url = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+
+    mylist = zip(news, desc, img, pubAt, url)
+
+    return render_template('Axios.html', context=mylist)
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
